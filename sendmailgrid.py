5acd450dca98b354469621b47bbf4640a284c7d4
Mazen Elzanaty <mazen.elzanaty@rootgate.com>
2020-01-22 01:21:46 +0200

    second

--- sendmailgrid.py
+++ sendmailgrid.py
@@ -5,26 +5,29 @@ import base64
 from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
 import shutil
 
+f = open("link.txt", "r")
+data=f.read()
+
 message = Mail(
     from_email=os.environ.get('FROM_EMAIL'),
     to_emails=os.environ.get('TO_EMAILS'),
     subject=os.environ.get('SUBJECT'),
-    html_content='<strong>Build is finished, check attached</strong>')
+    html_content='<strong>Build is finished, check the following link</strong> '+ 'data')
 
 
-shutil.make_archive('app', 'zip', os.environ.get('ATTACHMENT'))
-with open('app.zip', 'rb') as f:
-    data = f.read()
-    f.close()
-encoded_file = base64.b64encode(data).decode()
+#shutil.make_archive('app', 'zip', os.environ.get('ATTACHMENT'))
+#with open('app.zip', 'rb') as f:
+#    data = f.read()
+#    f.close()
+#encoded_file = base64.b64encode(data).decode()
 
-attachedFile = Attachment(
-    FileContent(encoded_file),
-    FileName('app.zip'),
-    FileType('application/zip'),
-    Disposition('attachment')
-)
-message.attachment = attachedFile
+#attachedFile = Attachment(
+#    FileContent(encoded_file),
+#    FileName('app.zip'),
+#    FileType('application/zip'),
+#    Disposition('attachment')
+#)
+#message.attachment = attachedFile
 
 try:
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
