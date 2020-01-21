b07a1b3a0a6a0488baba0039d8b11e18e7c35eb9
Mazen Elzanaty <mazen.elzanaty@rootgate.com>
2020-01-21 21:09:30 +0200

    first commit

--- sendmailgrid.py
+++ sendmailgrid.py
@@ -3,12 +3,11 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 message = Mail(
-    from_email='no-reply@code95.info',
-    to_emails='mazen.elzanaty@rootgate.com',
-    subject='Testing mail from api',
-    html_content='<strong>and ea sdajasndjsa sy to do anywhere, even with Python</strong>')
+    from_email='FROM_EMAIL',
+    to_emails='TO_EMAILS',
+    subject='SUBJECT',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
-     SENDGRID_API_KEY=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
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
